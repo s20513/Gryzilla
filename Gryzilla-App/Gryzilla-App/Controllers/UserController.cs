8de5f068354fe6a84d281d945ef0d5dd94035cb9
s20513 <60384820+s20513@users.noreply.github.com>
2022-04-22 14:52:07 +0200

    addedDelete (#9)

--- Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
+++ Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
@@ -3,12 +3,21 @@
 namespace Gryzilla_App.Controllers;
 
 [ApiController]
-[Route("api")]
+[Route("api/users")]
 public class UserController : Controller
 {
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteUser([FromRoute] int id)
+    {
+        return Ok("usunieto");
+    }
+
+
     [HttpGet("{idUser:int}")]
     public async Task<IActionResult> GetUser([FromRoute] int idUser)
     {
         return Ok("Wszystko super");
     }
+
 }
