c2a459a54f07e26f49b1c33cd96fae17001e0af2
s20660 <81235069+s20660@users.noreply.github.com>
2022-04-22 19:09:02 +0200

    zmiana koncowki

--- Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
+++ Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
@@ -37,7 +37,7 @@ public class UserController : Controller
     [HttpPost("{idUser:int}")]
     public async Task<IActionResult> PostNewUser([FromRoute] int idUser)
     {
-        return Ok("add-pp");
+        return Ok("add-ppp");
     }
     
     [HttpDelete("{id:int}")]
