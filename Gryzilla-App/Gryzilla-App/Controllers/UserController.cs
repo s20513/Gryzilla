083dae3fcb91f6981b15908ab3c5db1929758a2d
s20509 <80327149+s20509@users.noreply.github.com>
2022-04-22 14:50:20 +0200

    add get

--- Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
+++ Gryzilla-App/Gryzilla-App/Controllers/UserController.cs
@@ -6,5 +6,9 @@ namespace Gryzilla_App.Controllers;
 [Route("api")]
 public class UserController : Controller
 {
-    
+    [HttpGet("{idUser:int}")]
+    public async Task<IActionResult> GetUser([FromRoute] int idUser)
+    {
+        return Ok("Wszystko super");
+    }
 }
