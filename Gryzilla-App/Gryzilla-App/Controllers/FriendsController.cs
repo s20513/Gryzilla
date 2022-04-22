41406fb8e7a6d0d9ec5118ef9dcd057393071fd5
s20509 <80327149+s20509@users.noreply.github.com>
2022-04-22 19:58:21 +0200

    changes

--- Gryzilla-App/Gryzilla-App/Controllers/FriendsController.cs
+++ Gryzilla-App/Gryzilla-App/Controllers/FriendsController.cs
@@ -1,12 +1,21 @@
 ﻿using Gryzilla_App.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Gryzilla_App.Controllers;
 
-public class FriendsController
+[ApiController]
+[Route("api/friends")]
+public class FriendsController : Controller
 {
-    private readonly IFriendsDbRepository _userDbRepository;
-    public FriendsController(IFriendsDbRepository userDbRepository)
+    private readonly IFriendsDbRepository _friendsDbRepository;
+    public FriendsController(IFriendsDbRepository friendsDbRepository)
     {
-        _userDbRepository = userDbRepository;
+        _friendsDbRepository = friendsDbRepository;
+    }
+
+    [HttpGet("{idUser:int}")]
+    public async Task<IActionResult> GetFriends([FromRoute] int idUser)
+    {
+        return Ok("dodano");
     }
 }
