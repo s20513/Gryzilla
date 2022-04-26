f497d64fa6eb91b68c01ca688d26d4a093242681
s20509 <80327149+s20509@users.noreply.github.com>
2022-04-27 00:47:37 +0200

    added get posts

--- Gryzilla-App/Gryzilla-App/Repositories/Interfaces/IPostDbRepository.cs
+++ Gryzilla-App/Gryzilla-App/Repositories/Interfaces/IPostDbRepository.cs
@@ -5,6 +5,10 @@ namespace Gryzilla_App.Repositories.Interfaces;
 public interface IPostDbRepository
 {
     public Task<IEnumerable<PostDto>?> GetPostsFromDb();
+    public Task<IEnumerable<PostDto>?> GetPostsByLikesFromDb();
+    public Task<IEnumerable<PostDto>?> GetPostsByLikesLeastFromDb();
+    public Task<IEnumerable<PostDto>?> GetPostsByDateFromDb();
+    public Task<IEnumerable<PostDto>?> GetPostsByDateOldestFromDb();
     public Task<string?> AddNewPostFromDb(AddPostDto addPostDto);
     public Task<string?> DeletePostFromDb(int idPost);
 }
