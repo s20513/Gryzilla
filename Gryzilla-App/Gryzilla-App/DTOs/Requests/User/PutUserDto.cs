69cc3d1212073ae940d8f4480a673652cfdcad67
s20660 <81235069+s20660@users.noreply.github.com>
2022-05-05 02:41:49 +0200

    Dodanie PutUser

---
+++ Gryzilla-App/Gryzilla-App/DTOs/Requests/User/PutUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gryzilla_App.DTO.Requests.User;
+
+public class PutUserDto
+{
+    [Required]
+    [MaxLength(30,ErrorMessage = "Max length : 30")]
+    public string Nick { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Phone]
+    public string PhoneNumber { get; set; }
+}
