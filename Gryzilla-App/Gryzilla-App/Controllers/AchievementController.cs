78c41ae64ac24f556b009b8fd2bdacd057a2daed
Mateusz Skonieczny <s20513@pjwstk.edu.pl>
2022-10-22 16:19:56 +0200

    Refactored AchievementController.cs

--- Gryzilla-App/Gryzilla-App/Controllers/AchievementController.cs
+++ Gryzilla-App/Gryzilla-App/Controllers/AchievementController.cs
@@ -17,9 +17,14 @@ public class AchievementController : Controller
     [HttpGet]
     public async Task<IActionResult> GetAchievements()
     {
-        var achievementsFrom= await _achievementDbRepository.GetAchievementsFromDb();
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .GetAchievementsFromDb();
+
         if (achievementsFrom is null)
+        {
             return NotFound("Not found any achievements");
+        }
         
         return Ok(achievementsFrom);
     }
@@ -28,19 +33,32 @@ public class AchievementController : Controller
     public async Task<IActionResult> ModifyAchievement([FromRoute] int idAchievement, [FromBody] PutAchievementDto putAchievementDto)
     {
         if (idAchievement != putAchievementDto.IdAchievement)
+        {
             return BadRequest();
-        var achievementsFrom= await _achievementDbRepository.ModifyAchievement(idAchievement,putAchievementDto);
+        }
+            
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .ModifyAchievement(idAchievement, putAchievementDto);
+
         if (achievementsFrom is null)
+        {
             return NotFound("Cannot modify achievement");
+        }
         
         return Ok(achievementsFrom);
     }
 
     [HttpPost]
     public async Task<IActionResult> AddNewAchievement([FromBody] AddAchievementDto addAchievementDto ){
-        var achievementsFrom= await _achievementDbRepository.AddNewAchievement(addAchievementDto);
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .AddNewAchievement(addAchievementDto);
+
         if (achievementsFrom is null)
+        {
             return NotFound("Cannot add new achievement");
+        }
         
         return Ok(achievementsFrom);
     }
@@ -48,20 +66,28 @@ public class AchievementController : Controller
     [HttpDelete("{idAchievement:int}")]
     public async Task<IActionResult> DeleteAchievement([FromRoute] int idAchievement)
     {
-        var achievementsFrom= await _achievementDbRepository.DeleteAchievement(idAchievement);
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .DeleteAchievement(idAchievement);
+
         if (achievementsFrom is null)
+        {
             return NotFound("Cannot delete achievement");
+        }
         
         return Ok(achievementsFrom);
-       
     }
     
     [HttpPost("{idAchievement:int}/{idUser:int}")]
     public async Task<IActionResult> AddNewUserAchievement([FromRoute] int idAchievement, [FromRoute] int idUser ){
-        var achievementsFrom= await _achievementDbRepository.AddNewUserAchievement(idAchievement,idUser);
-        
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .AddNewUserAchievement(idAchievement,idUser);
+
         if (achievementsFrom is null)
+        {
             return NotFound("Cannot add achievement to user");
+        }
         
         return Ok(achievementsFrom);
     }
@@ -69,9 +95,14 @@ public class AchievementController : Controller
     [HttpDelete("{idAchievement:int}/{idUser:int}")]
     public async Task<IActionResult> DeleteUserAchievement([FromRoute] int idAchievement, [FromRoute] int idUser )
     {
-        var achievementsFrom= await _achievementDbRepository.DeleteUserAchievement(idAchievement, idUser);
+        var achievementsFrom= 
+            await _achievementDbRepository
+                .DeleteUserAchievement(idAchievement, idUser);
+
         if (achievementsFrom is null)
+        {
             return NotFound("Cannot delete achievement from user");
+        }
         
         return Ok(achievementsFrom);
        
