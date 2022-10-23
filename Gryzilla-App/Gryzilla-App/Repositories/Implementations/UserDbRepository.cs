ff2f5bab4a95cbdce56a4e629d016aaeb54f9626
s20509 <s20509@pjwstk.edu.pl>
2022-10-23 18:50:45 +0200

    Update UserDbRepository.cs

--- Gryzilla-App/Gryzilla-App/Repositories/Implementations/UserDbRepository.cs
+++ Gryzilla-App/Gryzilla-App/Repositories/Implementations/UserDbRepository.cs
@@ -84,7 +84,7 @@ public class UserDbRepository : IUserDbRepository
         }
     }
 
-    private Exception? Exception { get; }
+    private Exception? NameTakenException { get; }
     public async Task<UserDto?> AddUserToDb(AddUserDto addUserDto)
     {
         var nick = await _context.UserData
@@ -93,7 +93,7 @@ public class UserDbRepository : IUserDbRepository
         
         if (nick is not null)
         {
-            throw Exception;
+            throw NameTakenException!;
         }
         
         var newUser = new UserDatum
