e8ced6b2e5349f443a45c7f997dee7c19ae85687
s20509 <s20509@pjwstk.edu.pl>
2022-10-23 18:46:40 +0200

    Fixed userRepository and userController

--- Gryzilla-App/Gryzilla-App/DTOs/Requests/User/AddUserDto.cs
+++ Gryzilla-App/Gryzilla-App/DTOs/Requests/User/AddUserDto.cs
@@ -1,17 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
-namespace Gryzilla_App.DTO.Requests.User;
+namespace Gryzilla_App.DTOs.Requests.User;
 
 public class AddUserDto
 {
     [Required]
     [MaxLength(30,ErrorMessage = "Max length : 30")]
-    public string Nick { get; set; }
+    public string Nick { get; set; } = null!;
+
+    [Required]
+    [PasswordPropertyText]
+    [MaxLength(30,ErrorMessage = "Max length : 30")]
+    public string Password { get; set; } = null!;
 
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
-    
+    public string Email { get; set; } = null!;
+
     [Phone]
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = null!;
 }
--- Gryzilla-App/Gryzilla-App/Repositories/Interfaces/IUserDbRepository.cs
+++ Gryzilla-App/Gryzilla-App/Repositories/Interfaces/IUserDbRepository.cs
@@ -1,5 +1,5 @@
-﻿using Gryzilla_App.DTO.Requests.User;
-using Gryzilla_App.DTO.Responses;
+﻿using Gryzilla_App.DTOs.Requests.User;
+using Gryzilla_App.DTOs.Responses.User;
 
 namespace Gryzilla_App.Repositories.Interfaces;
 
@@ -8,7 +8,7 @@ public interface IUserDbRepository
      public Task<UserDto?> GetUserFromDb(int idUser);
      public Task<IEnumerable<UserDto?>> GetUsersFromDb();
      public Task<UserDto?> ModifyUserFromDb( int idUser, PutUserDto putUserDto);
-     
-     
+     public Task<UserDto?> AddUserToDb(AddUserDto addUserDto);
+     public Task<UserDto?> DeleteUserFromDb(int idUser);
      
 }
