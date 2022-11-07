e55b9a562b0244a043dee3f77afa5a9926f7f2c0
Mateusz Skonieczny <s20513@pjwstk.edu.pl>
2022-11-07 20:05:57 +0100

    Added ModifyAchievement_Returns_Modified_Achievement test

--- Gryzilla-App/UnitTest/AchievementControllerTests.cs
+++ Gryzilla-App/UnitTest/AchievementControllerTests.cs
@@ -1,5 +1,6 @@
 using FakeItEasy;
 using Gryzilla_App.Controllers;
+using Gryzilla_App.DTO.Requests.Rank;
 using Gryzilla_App.DTOs.Responses.Achievement;
 using Gryzilla_App.Models;
 using Gryzilla_App.Repositories.Interfaces;
@@ -48,5 +49,29 @@ public class AchievementControllerTests
         
         Assert.Equal("Not found any achievements", returnAchievements);
 
+    }
+
+    [Fact]
+    public void ModifyAchievement_Returns_Modified_Achievement()
+    {
+        //Arrange
+        //var achievement = A.CollectionOfDummy<AchievementDto>(count).AsEnumerable();
+        var id = 5;
+        var achievement = A.Dummy<PutAchievementDto>();
+        achievement.IdAchievement = 5;
+        var repository = A.Fake<IAchievementDbRepository>();
+        var modifiedAchievement = A.Dummy<AchievementDto>();
+        modifiedAchievement.Description = "test";
+        A.CallTo(() => repository.ModifyAchievement(id, achievement)).Returns(Task.FromResult(modifiedAchievement));
+        var controller = new AchievementController(repository);
+        
+        //Act
+        var actionResult = controller.ModifyAchievement(id, achievement);
+        
+        //Assert
+        var result = actionResult.Result as OkObjectResult;
+        var returnAchievements = result.Value as AchievementDto;
+        
+        Assert.Equal("test", returnAchievements.Description);
     }
 }
