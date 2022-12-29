0edd82f500b192d52cdf2c20d54e737144a7e795
s20660 <81235069+s20660@users.noreply.github.com>
2022-12-29 15:26:53 +0100

    Updated batch file

--- publish.bat
+++ publish.bat
@@ -6,13 +6,15 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`git pull`) DO (
 SET var=%%F
 )
 
-ECHO  zmienna to %var%
+ECHO --Pull result: %var%
+IF "%var%" == "Already up to date." (
+ ECHO -- no further action required
+) ELSE (
+ ECHO -- Changing directory
+ cd Gryzilla-App
 
-ECHO -- Changing directory
-cd Gryzilla-App
+ ECHO -- Publishing Gryzilla to local files
+ dotnet publish --output ./../Published --runtime win-x64 -p:PublishSingleFile=true --self-contained true --configuration Release
+)
 
-ECHO -- Publishing Gryzilla to local files
-dotnet publish --output ./../Published --runtime win-x64 -p:PublishSingleFile=true --self-contained true --configuration Release
-
-ECHO Done. Files in Published folder, run .exe to execute ;)
 PAUSE
