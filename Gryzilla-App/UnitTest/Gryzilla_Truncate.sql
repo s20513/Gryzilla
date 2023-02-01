887f24b2ea96a9c141c000f486252bb74d46940e
Mateusz Skonieczny <s20513@pjwstk.edu.pl>
2023-02-01 09:45:02 +0100

    Added truncating temporal table

---
+++ Gryzilla-App/UnitTest/Gryzilla_Truncate.sql
@@ -0,0 +1,63 @@
+TRUNCATE TABLE [Friends]
+
+ALTER TABLE dbo.BlockedUser SET (SYSTEM_VERSIONING = OFF);
+
+TRUNCATE TABLE BlockedUser
+TRUNCATE TABLE BlockedUser_history
+
+ALTER TABLE dbo.BlockedUser
+SET(SYSTEM_VERSIONING = ON (HISTORY_TABLE = dbo.BlockedUser_History, DATA_CONSISTENCY_CHECK = ON))
+
+TRUNCATE TABLE [AchievementUser]
+
+DELETE FROM [Achievement]
+DBCC CHECKIDENT ([Achievement], RESEED, 0)
+
+TRUNCATE TABLE [Blocked]
+
+TRUNCATE TABLE [Notification]
+
+TRUNCATE TABLE [ProfileComment]
+
+TRUNCATE TABLE [GroupUser]
+
+DELETE FROM [Group]
+DBCC CHECKIDENT ([Group], RESEED, 0)
+
+TRUNCATE TABLE [LikePost]
+
+TRUNCATE TABLE [LikeArticle]
+
+TRUNCATE TABLE [PostTag]
+
+TRUNCATE TABLE [ArticleTag]
+
+DELETE FROM [Tag]
+DBCC CHECKIDENT ([Tag], RESEED, 0)
+
+TRUNCATE TABLE [ReportCommentArticle]
+
+TRUNCATE TABLE [ReportCommentPost]
+
+TRUNCATE TABLE [ReportPost]
+
+DELETE FROM [CommentPost]
+DBCC CHECKIDENT ([CommentPost], RESEED, 0)
+
+DELETE FROM [CommentArticle]
+DBCC CHECKIDENT ([CommentArticle], RESEED, 0)
+
+DELETE FROM [Reason]
+DBCC CHECKIDENT ([Reason], RESEED, 0)
+
+DELETE FROM [Post]
+DBCC CHECKIDENT ([Post], RESEED, 0)
+
+DELETE FROM [Article]
+DBCC CHECKIDENT ([Article], RESEED, 0)
+
+DELETE FROM [UserData]
+DBCC CHECKIDENT ([UserData], RESEED, 0)
+
+DELETE FROM [Rank]
+DBCC CHECKIDENT ([Rank], RESEED, 0)
