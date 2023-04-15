9134dd653a3049f926a7cfe07757dc3ed9035002
s20509 <s20509@pjwstk.edu.pl>
2023-04-15 13:59:10 +0200

    Group user message

---
+++ Gryzilla-App/Gryzilla-App/DTOs/Requests/GroupUserMessage/UpdateGroupUserMessageDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Gryzilla_App.DTOs.Requests.GroupUserMessage;
+
+public class UpdateGroupUserMessageDto
+{
+    public int IdMessage { get; set; }
+    public string Content { get; set; }
+}
---
+++ Gryzilla-App/Gryzilla-App/Repositories/Interfaces/IGroupUserMessageDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using Gryzilla_App.DTOs.Requests.GroupUserMessage;
+using Gryzilla_App.DTOs.Responses.GroupUserMessageDto;
+
+namespace Gryzilla_App.Repositories.Interfaces;
+
+public interface IGroupUserMessageDbRepository
+{
+    public  Task<GroupUserMessageDto?> DeleteMessage(int idGroupMessage);
+    public  Task<GroupUserMessageDto?> ModifyMessage(int idMessage, UpdateGroupUserMessageDto updateGroupUserMessage);
+
+    public  Task<GroupUserMessageDto?> AddMessage(AddGroupUserMessageDto addGroupUserMessage);
+    public  Task<GroupUserMessageDto[]?> GetMessages(int idGroup);
+}
